045d642f763e349bdf812a9f8b8c761b7c1ca3b0
jigspatel_it <jigspatel.04@gmail.com>
2020-11-05 20:05:35 +0530

    Build Cart Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -15,5 +15,4 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
     },
   });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-  console.log(JSON.parse(localStorage.getItem('cartItems')))
 };
