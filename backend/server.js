67c9401a27fd78465cd02ab26af94ddcb9953caa
jigspatel_it <jigspatel.04@gmail.com>
2020-11-06 19:52:06 +0530

    create sample users in mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res, next) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -16,8 +23,14 @@ app.get('/api/products/', (req, res, next) => {
   res.send(data.products);
 });
 
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res, next) => {
   res.send('Server is running');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
