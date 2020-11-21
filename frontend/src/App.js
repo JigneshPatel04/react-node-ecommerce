0febe7341169c7af22e00e06215f11588a02f586
jigspatel_it <jigspatel.04@gmail.com>
2020-11-21 20:59:16 +0530

    Create Payment Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import SigninScreen from './screens/SigninScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import { signout } from './actions/userActions';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -61,6 +62,7 @@ function App() {
           <Route path='/signin' component={SigninScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/shipping' component={ShippingAddressScreen} />
+          <Route path='/payment' component={PaymentMethodScreen} />
           <Route path='/' component={HomeScreen} exact />
         </main>
         <footer className='row center'>All right reserved</footer>
