34579a2a25aa7cdf0cbed9fb011ecda403471ea0
jigspatel_it <jigspatel.04@gmail.com>
2020-11-27 16:54:20 +0530

    Order and order history page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
@@ -23,50 +24,54 @@ function App() {
   };
   return (
     <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
+      <div className='grid-container'>
+        <header className='row'>
           <div>
-            <Link className="brand" to="/">
+            <Link className='brand' to='/'>
               amazona
             </Link>
           </div>
           <div>
-            <Link to="/cart">
+            <Link to='/cart'>
               Cart
               {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
+                <span className='badge'>{cartItems.length}</span>
               )}
             </Link>
             {userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+              <div className='dropdown'>
+                <Link to='#'>
+                  {userInfo.name} <i className='fa fa-caret-down'></i>{' '}
                 </Link>
-                <ul className="dropdown-content">
+                <ul className='dropdown-content'>
                   <li>
-                    <Link to="#signout" onClick={signoutHandler}>
+                    <Link to='/orderhistory'>Order History</Link>
+                  </li>
+                  <li>
+                    <Link to='#signout' onClick={signoutHandler}>
                       Sign Out
                     </Link>
                   </li>
                 </ul>
               </div>
             ) : (
-              <Link to="/signin">Sign In</Link>
+              <Link to='/signin'>Sign In</Link>
             )}
           </div>
         </header>
         <main>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/signin" component={SigninScreen}></Route>
-          <Route path="/register" component={RegisterScreen}></Route>
-          <Route path="/shipping" component={ShippingAddressScreen}></Route>
-          <Route path="/payment" component={PaymentMethodScreen}></Route>
-          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
-          <Route path="/order/:id" component={OrderScreen}></Route>
-          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/signin' component={SigninScreen}></Route>
+          <Route path='/register' component={RegisterScreen}></Route>
+          <Route path='/shipping' component={ShippingAddressScreen}></Route>
+          <Route path='/payment' component={PaymentMethodScreen}></Route>
+          <Route path='/placeorder' component={PlaceOrderScreen}></Route>
+          <Route path='/order/:id' component={OrderScreen}></Route>
+          <Route path='/orderhistory' component={OrderHistoryScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All right reserved</footer>
+        <footer className='row center'>All right reserved</footer>
       </div>
     </BrowserRouter>
   );
